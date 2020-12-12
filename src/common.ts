8688c2b14e6f564af6a450ca755b811ba2625757
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2020-12-12 21:20:27 +0100

    Fixed bug that special stars would not be displayed Fixes #23

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,4 @@
 export function parseDay(date: Date, day?: number): string {
   if (!day) day = date.getDate();
-  return date.getDate() < 10 ? `0${day}` : day.toString();
+  return day < 10 ? `0${day}` : day.toString();
 }
