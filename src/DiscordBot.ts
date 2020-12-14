ae5cfe2d2c2aae6057520035d963872cad2f9e68
LeMoonStar <webmaster@unitcore.de>
2020-12-14 18:18:53 +0100

    added DatabaseHelper to DiscordBot class

--- src/DiscordBot.ts
+++ src/DiscordBot.ts
@@ -1,13 +1,17 @@
 import * as Discord from 'discord.js';
 import ChannelUpdater from './ChannelUpdater';
+import DatabaseHelper from './DatabaseHelper';
 import Leaderboard from './Leaderboard';
 
 export default class DiscordBot {
   static _client: Discord.Client;
+  static _db: DatabaseHelper;
   constructor() {
-    DiscordBot._client = new Discord.Client();
-    DiscordBot._client.login(process.env.TOKEN).catch(console.error);
-    DiscordBot._client.on('ready', this.onReady.bind(this));
+    DiscordBot._db = new DatabaseHelper('configuration.sqlite', () => {
+      DiscordBot._client = new Discord.Client();
+      DiscordBot._client.login(process.env.TOKEN).catch(console.error);
+      DiscordBot._client.on('ready', this.onReady.bind(this));
+    });
   }
 
   private onReady(): void {
